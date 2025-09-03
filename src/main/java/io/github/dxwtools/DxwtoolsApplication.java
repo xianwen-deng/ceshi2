e65a8fd8d4d7aab6bafccd887e55b57fcfc209df
xianwen.deng <591278546@qq.com>
2025-09-03 13:11:51 +0800

    初始化

---
+++ src/main/java/io/github/dxwtools/DxwtoolsApplication.java
@@ -0,0 +1,13 @@
+package io.github.dxwtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DxwtoolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DxwtoolsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/dxwtools/DxwtoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dxwtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DxwtoolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
